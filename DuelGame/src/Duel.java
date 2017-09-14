6cbe465000dea093fa514bf84952eeda36ddb607
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-09-14 14:43:24 -0400

    Duel

--- DuelGame/src/Duel.java
+++ DuelGame/src/Duel.java
@@ -1,4 +1,54 @@
 
 public class Duel {
 
+	private Dueler d1;
+	private Dueler d2;
+	private int dueler1HP = 30;
+	private int dueler2HP = 30;
+	
+	
+	
+	
+	public static void main(String[] args){
+		Duel duel = new Duel();
+		duel.dukeItOut();
+	}
+	
+	public Duel() {
+		d1 = new CharacterA();
+		d2 = new CharacterB();
+	}
+	
+	public void dukeItOut(){
+		System.out.print(d1.getName());
+		pause(600);
+		System.out.print(" vs ");
+		pause(600);
+		System.out.println(d2.getName());
+		pause(600);
+		d1.taunt();
+		pause(2000);
+		d2.taunt();
+		pause(2000);
+		d1.setStartingHP(dueler1HP);
+		d2.setStartingHP(dueler2HP);
+		
+		
+
+		
+	}
+	
+	
+	/**
+	 * This method involves "Threads" which we will get to much later in the year, essentially, it
+	 * creates a pause in the execution of the program, which makes it more dramatic
+	 * @param time
+	 */
+	public void pause(long time){
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
