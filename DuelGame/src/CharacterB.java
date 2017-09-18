3676754b7b0490e4f11c57a31ec1252706810c60
Vicky Icky <Vicky Icky@VickyIcky>
2017-09-17 22:45:03 -0400

    B

--- DuelGame/src/CharacterB.java
+++ DuelGame/src/CharacterB.java
@@ -1,16 +1,21 @@
 
 public class CharacterB implements Dueler
 {
+	private String name;
+	private int hp;
+	
+	public CharacterB () {}
 	
 	public void taunt()
 	{
-		String output =  + ; //use spaces in btwn operations EX: 1 + 1 = 2; NOT 1+1=2
-		System.out.println(output);
+		//String output =  + ; //use spaces in btwn operations EX: 1 + 1 = 2; NOT 1+1=2
+		//System.out.println(output);
 	}
 	
 	public String getName()
 	{
-		
+		name = "Vickie";
+		return name;
 	}
 	
 	public void setStartingHP(int hp)
