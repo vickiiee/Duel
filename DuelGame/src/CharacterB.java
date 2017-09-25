589f86ef387c0c390fe159f2c46c0f9cfd21ad88
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-09-25 14:39:20 -0400

    B

--- DuelGame/src/CharacterB.java
+++ DuelGame/src/CharacterB.java
@@ -3,14 +3,14 @@ public class CharacterB implements Dueler
 {
 	private String name;
 	private int hp;
-	private String[] taunt = {"Who'll win? I'LL WIN!!", "The champion is here!!!", "Hahahahahahahahahaaaaa, Do you really think you'll be the winner"};
+	private String[] taunt = {"The champion is here!!!", "Who'll win? I'LL WIN!!", "Hahahahahahahahahaaaaa, Do you really think you'll be the winner"};
 	
 	public CharacterB () {}
 	
 	public void  taunt()
 	{
-		int randNum = (int)Math.random()*2;
-		System.out.println(taunt[randNum]);
+		//int randNum = (int)Math.random()*3;
+		System.out.println(taunt[1]);
 	}
 	
 	public String getName()
