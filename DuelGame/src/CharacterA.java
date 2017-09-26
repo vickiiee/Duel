8cae8a23274363b9e2bc4adb5eeb60ac07e0f7e7
Vicky Icky <Vicky Icky@VickyIcky.fios-router.home>
2017-09-26 01:06:01 -0400

    A

--- DuelGame/src/CharacterA.java
+++ DuelGame/src/CharacterA.java
@@ -6,36 +6,39 @@ public class CharacterA implements Dueler
 	private String[] taunt = {"You don't stand a chance!" , "I'm going to beat you", "Hahaha you're going to lose"};
 	private boolean loadedGun;
 	
-	public CharacterA () {}
+	public CharacterA () 
+	{
+		loadedGun = false;
+	}
 	
-	public void taunt() {
-		
+	public void taunt()
+	{
 		int randNum = (int)(Math.random()*2)+1;
 		System.out.println(taunt[randNum]);
 	}
 	
 	public String getName()
 	{
-		name = "Me";
+		name = "Areej";
 		return name;
 	}
 	
-	public void setStartingHP(int h) {
-		
-		System.out.println(h);
+	public void setStartingHP(int hp)
+	{
+		this.hp = hp;
 	}
 	
-	public int getHP() {
-		
+	public int getHP()
+	{
 		return this.hp;
 	}
-	
-	public boolean determineIfOpponentIsFair(Dueler d, int target) {
+
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
 		if (d.getHP() == hp)
 		{
 			return true;
-		}
-		else 
+		} 
+		else
 		{
 			return false;
 		}
@@ -45,30 +48,36 @@ public class CharacterA implements Dueler
 	{
 		if(caller instanceof Duel)
 		{
-		
-			
-			if(loadedGun == true && Math.random() < .5) {
-				
+			if(loadedGun == true && Math.random() < .5) 
+			{
 				loadedGun = false;
 				return Duel.SHOOTING;
 			}
-			
-			if(!loadedGun && Math.random() < .5) {
-				
-				loadedGun = true;
-				return Duel.LOADING;
-			}
-			else
+			else 
 			{
-				return Duel.GUARDING;
+				if(!loadedGun && Math.random() < .4) 
+				{
+					loadedGun = true;
+					return Duel.LOADING;
+				}
+				
+				else
+				{
+					return Duel.GUARDING;
+				}
 			}
 		}
-		
-		else {
+		else 
+		{
 			return Duel.YEAH_RIGHT;
 		}
 	}
 	
-	
+	public void hit(Object caller)
+	{
+		if(caller instanceof Duel)
+		{
+			this.hp = this.hp - 10;
+		}
+	}
 }
-
